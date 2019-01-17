4de31dc015b680ed9ac90a78124ec3b8d8dceb67
Natalia <nat.kaseeva@gmail.com>
2019-01-17 20:51:38 +0400

    structure-part-1

---
+++ www/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+// Хост сайта
+define('HOST', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/');
+
+// Физический путь к корневой директории скрипта
+define('ROOT', dirname(__FILE__).'/');
+
+
+?>
--- www/index.php
+++ www/index.php
@@ -1,5 +1,38 @@
 <?php 
 
-echo "Hello!";
+require "config.php";
+
+/* ..........................................
+
+РОУТЕР
+
+............................................. */
+$uri =  $_SERVER["REQUEST_URI"];
+$uri = rtrim($uri, "/"); 
+$uri = filter_var($uri, FILTER_SANITIZE_URL);
+$uri = substr($uri, 1);
+$uri = explode('?', $uri);
+
+switch ( $uri[0]) {
+	case '':
+		include "modules/main/index.php";
+		break;
+
+	case 'about':
+		include "modules/about/index.php";
+		break;
+
+	case 'contacts':
+		include "modules/contacts/index.php";
+		break;
+
+	case 'blog':
+		include "modules/blog/index.php";
+		break;
+
+	default:
+		include "modules/main/index.php";
+		break;
+}
 
  ?>
